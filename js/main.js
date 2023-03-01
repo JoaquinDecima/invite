7cdaf15fbc50c43ec01014ab7ec10263a5f72a19
Joaquin (Pato) Decima <jdecima@bancodelsol.com>
2023-03-01 16:21:18 -0300

    Add Contdawn

--- js/main.js
+++ js/main.js
@@ -3,3 +3,10 @@ new fullpage("#fullpage", {
   autoScrolling: true,
   scrollHorizontally: true,
 });
+
+const timer = new CountdownTimer({
+  selector: "#clock1",
+  targetDate: new Date("March, 05 2023 16:00:00"),
+});
+
+timer.startTimer();
