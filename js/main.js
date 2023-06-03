2ba41a28742a5c8a65e2c27b0d2eeac300982eec
Joaquin (Pato) Decima <jdecima@bancodelsol.com>
2023-06-03 20:48:34 -0300

    tarjeta martina

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-new fullpage("#fullpage", {
-  //options here
-  autoScrolling: true,
-  scrollHorizontally: true,
+new fullpage('#fullpage', {
+	//options here
+	autoScrolling: true,
+	scrollHorizontally: true,
 });
 
 const timer = new CountdownTimer({
-  selector: "#clock1",
-  targetDate: new Date("March, 12 2023 11:00:00"),
+	selector: '#clock1',
+	targetDate: new Date('July, 08 2023 20:00:00'),
 });
 
 timer.startTimer();
